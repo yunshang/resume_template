16c3cd5e5cbf757458c460123b49334b7eedd240
yunshang <alshin@126.com>
2020-04-20 18:30:12 +0800

    Add config

--- src/sagaDucks/rsf.tsx
+++ src/sagaDucks/rsf.tsx
@@ -1,17 +1,12 @@
 import firebase from 'firebase';
 import ReduxSagaFirebase from 'redux-saga-firebase';
+import config  from  '../../config';
 
-// const firebaseApp = firebase.initializeApp({
-//   apiKey: 'AIzaSyCe-AgS6huR3tqHYQ-hXUejrMSQDiC44vE',
-//   authDomain: 'resume-data.firebaseapp.com',
-//   databaseURL: 'https://resume-data.firebaseio.com',
-//   projectId: 'resume-data',
-// });
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyBFMzaucx4Bz26SafXnR-P3IU13cKUOvSA',
-  authDomain: 'resume-template-db588.firebaseio.com',
-  databaseURL: 'https://resume-template-db588.firebaseio.com',
-  projectId: 'resume-template-db588',
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  projectId: config.projectId,
 });
 
 const rsf = new ReduxSagaFirebase(firebaseApp);
