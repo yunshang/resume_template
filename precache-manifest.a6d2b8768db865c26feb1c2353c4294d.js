d344924e68cf6a4645cb276caa987d7f6af16cee
alshin <alshin@126.com>
2021-03-09 00:15:36 +0800

    Updates

---
+++ precache-manifest.a6d2b8768db865c26feb1c2353c4294d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a954e18e362bfe18197385473027b7e",
+    "url": "/resume_template/index.html"
+  },
+  {
+    "revision": "0d1c5314fa9b9e4cc21b",
+    "url": "/resume_template/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "1a4c93522013ec5cc626",
+    "url": "/resume_template/static/js/0.a4e2bf45.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/resume_template/static/js/0.a4e2bf45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c991aae673deb68275",
+    "url": "/resume_template/static/js/1.15460be5.chunk.js"
+  },
+  {
+    "revision": "6c5ab9b389ab37603b4f",
+    "url": "/resume_template/static/js/2.106bea01.chunk.js"
+  },
+  {
+    "revision": "92ca21309cb271f0ea30",
+    "url": "/resume_template/static/js/5.6a834b62.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/resume_template/static/js/5.6a834b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d9fe32002ca7c191d9",
+    "url": "/resume_template/static/js/6.7a4df015.chunk.js"
+  },
+  {
+    "revision": "0d1c5314fa9b9e4cc21b",
+    "url": "/resume_template/static/js/main.74fba6ca.chunk.js"
+  },
+  {
+    "revision": "0a20f5762ad86faac667",
+    "url": "/resume_template/static/js/runtime-main.a96e23ba.js"
+  }
+]);
